7295f1862265a7cd4874fafe575b0fe02e173e82
HellBrick <hellbrick@outlook.com>
2019-04-21 22:07:41 +0300

    Added a test to verify that a real-world save file can be roundtripped correctly

---
+++ SDK/Aow.Test/Maps/MapTest.cs
@@ -0,0 +1,20 @@
+﻿using Aow.Test.Maps.Resources;
+using Aow2.Maps;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aow2.Test.Maps
+{
+	[TestClass]
+	public class MapTest
+	{
+		[TestMethod]
+		public void SaveRoundTrips()
+		{
+			AowMap original = AowMap.FromBytes( MapFiles.Save );
+			AowMap roundTripped = AowMap.FromBytes( original.ToBytes() );
+
+			roundTripped.Should().BeEquivalentTo( original );
+		}
+	}
+}
