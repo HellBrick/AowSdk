0c4dc5fef35411587aa9e1be95dd928f555bdd8a
HellBrick <hellbrick@outlook.com>
2020-10-04 00:04:00 +0300

    Added Utils.WPF

---
+++ Utils/Utils.WPF/ContentResourceHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Utils.WPF
+{
+	public class ContentResourceHelper: DependencyObject
+	{
+		public static readonly DependencyProperty ContentKeyProperty = DependencyProperty.RegisterAttached(
+			   "ContentKey",
+			   typeof( string ),
+			   typeof( ContentResourceHelper ),
+			   new PropertyMetadata( ( s, e ) => OnPropertyChanged( s, e ) ) );
+
+		public static object GetContentKey( DependencyObject obj ) => obj.GetValue( ContentKeyProperty );
+
+		public static void SetContentKey( DependencyObject obj, object value ) => obj.SetValue( ContentKeyProperty, value );
+
+		private static void OnPropertyChanged( DependencyObject s, DependencyPropertyChangedEventArgs e )
+		{
+			if ( e.NewValue != null )
+			{
+				object key = e.NewValue;
+				ContentControl contentPresenter = s as ContentControl;
+
+				if ( contentPresenter != null && key != null )
+				{
+					object content = contentPresenter.TryFindResource( key );
+					if ( content != null )
+						contentPresenter.Content = content;
+				}
+			}
+		}
+	}
+}
---
+++ Utils/Utils.WPF/Converters/NullToVisibility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace Utils.WPF.Converters
+{
+	public class NullToVisibility: ConverterBase<object>
+	{
+		public NullToVisibility()
+		{
+			IfNull = Visibility.Collapsed;
+			IfNotNull = Visibility.Visible;
+		}
+
+		public Visibility IfNull { get; set; }
+		public Visibility IfNotNull { get; set; }
+
+		protected override object Convert( object value, Type targetType, object param, System.Globalization.CultureInfo culture )
+		{
+			if ( value == null )
+				return IfNull;
+			else
+				return IfNotNull;
+		}
+
+		protected override object ConvertBack( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture ) => throw new NotImplementedException();
+	}
+}
