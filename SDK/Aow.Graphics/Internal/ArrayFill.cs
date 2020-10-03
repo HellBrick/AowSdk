1746035d746ffc09801c3efee6ccec98d7b56ef2
HellBrick <hellbrick@outlook.com>
2020-10-03 23:54:40 +0300

    Added missing ArrayFill

---
+++ SDK/Aow.Graphics/Internal/ArrayFill.cs
@@ -0,0 +1,47 @@
+﻿using System.Security;
+
+namespace Utils.Collections
+{
+	static class ArrayExtensions
+	{
+		[SecuritySafeCritical]
+		public unsafe static void Fill( this byte[,] arr, byte value )
+		{
+			fixed ( byte* ptr = &arr[0, 0] )
+			{
+				for ( int i = 0; i < arr.Length; i++ )
+					*( ptr + i ) = value;
+			}
+		}
+
+		[SecuritySafeCritical]
+		public unsafe static void Fill( this sbyte[,] arr, sbyte value )
+		{
+			fixed ( sbyte* ptr = &arr[0, 0] )
+			{
+				for ( int i = 0; i < arr.Length; i++ )
+					*( ptr + i ) = value;
+			}
+		}
+
+		[SecuritySafeCritical]
+		public unsafe static void Fill( this short[,] arr, short value )
+		{
+			fixed ( short* ptr = &arr[0, 0] )
+			{
+				for ( int i = 0; i < arr.Length; i++ )
+					*( ptr + i ) = value;
+			}
+		}
+
+		[SecuritySafeCritical]
+		public unsafe static void Fill( this ushort[,] arr, ushort value )
+		{
+			fixed ( ushort* ptr = &arr[0, 0] )
+			{
+				for ( int i = 0; i < arr.Length; i++ )
+					*( ptr + i ) = value;
+			}
+		}
+	}
+}
